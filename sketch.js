ae989a98a4360d8231e76832ea1d691441149aa3
anton <kadjaiev.anton@gmail.com>
2018-05-08 15:51:26 +0200

    CHANGE: Game improvement

--- sketch.js
+++ sketch.js
@@ -1,33 +1,55 @@
 let height = 480;
 let width = 640;
+let area;
+let areaSize = width*20
 let foods = [];
+
 
 function setup() {
   createCanvas(width, height);
   slither = new slither(0, 0);
-  for(let i = 0; i < 50; i++){
-    let x = random(-width, width);
-    let y = random(-height, height);
-    foods[i] = new food(x, y);
+  for(let i = 0; i < 5000; i++){
+    this.generateFood();
+  }
+  area = createVector(0,0);
+}
+
+function generateFood(){
+  let x = random(-width*10, width*10);
+  let y = random(-width*10, width*10);
+  let pos = createVector(x, y);
+
+  if(p5.Vector.dist(pos, createVector(0, 0)) >= (areaSize/2 - 20)) {
+    this.generateFood()
+  } else {
+    foods.push(new food(x, y));
   }
 }
 
 function draw() {
+
   background(0);
 
   //translate(width / 2 - slither.position.x, height / 2 -slither.position.y)
   translate(width / 2, height / 2);
   //scale(10 / (slither.size));
-  translate(-slither.position.x, -slither.position.y);
-
+  if(slither.body.length != 0){
+    translate(-slither.body[0].position.x, -slither.body[0].position.y);
+  }
+  ellipse(area.x, area.y, areaSize, areaSize);
   slither.show();
   slither.update();
 
   for(let i = foods.length - 1 ; i >= 0 ; i-- ) {
-    foods[i].show();
+    foodDist = p5.Vector.dist(slither.body[0].position, foods[i].position);
+    if(foodDist < width){
+      foods[i].show();
+    }
     if(slither.eats(foods[i])){
       foods.splice(i, 1);
+      this.generateFood();
     }
 
   }
+
 }
--- slither.js
+++ slither.js
@@ -3,39 +3,63 @@ class slither {
   constructor(x, y) {
     this.size = 50;
     this.length = 10;
-    this.body = []
-    let head = new disk(x, y);
-    this.body[0] = head;
+    this.body = [];
+    this.initSnake(x, y);
+  }
+
+  initSnake(x, y) {
+    console.log("2")
+    let offset = 0;
+    for(let i = 0; i < 5*this.length; i++){
+      this.body[i] = new disk(x, y+offset);
+      offset += 2;
+    }
   }
 
   update() {
     let velocity = createVector(mouseX-width/2, mouseY-height/2);
     velocity.setMag(3);
-    this.position.add(velocity);
+    /*
+    let a = p5.Vector.dist(this.body[0].position, this.body[49].position);
+    let b = p5.Vector.dist(this.body[25].position, this.body[0].position);
+    let c = p5.Vector.dist(this.body[25].position, this.body[49].position);
+    let angle = (-sq(a) + sq(b) + sq(c))/(2*b*c);
+    console.log(angle)
+    */
+    this.body[0].position.add(velocity);
+    this.body.unshift(new disk(this.body[0].position.x, this.body[0].position.y));
+    if(this.body.length > 5*this.length){
+      this.body.pop();
+    }
   }
+
+
 
   show(){
      fill(255);
-     this.drawCircles(this.position.x, this.position.y)
+     this.drawCircles();
   }
 
   eats(food) {
-    let dist = p5.Vector.dist(this.position, food.position);
+    let dist = p5.Vector.dist(this.body[0].position, food.position);
     if (dist < (this.size + food.size)/2) {
       return true;
     } else {
       return false;
     }
   }
-  disk(x, y){
-    this.position = createVector(x, y)
-  }
-  drawCircles(x, y){
-    let offset = 0;
-    for(let i = 0; i < this.length; i++){
-      ellipse(x, y + offset, this.size, this.size);
-      offset += 15;
-    }
 
+  drawCircles(){
+    if(this.body.length != 0){
+      for(let i = this.body.length - 1; i >= 0; i-=5){
+        ellipse(this.body[i].position.x, this.body[i].position.y, this.size, this.size);
+      }
+    }
+  }
+}
+
+class disk{
+  constructor(x, y) {
+    this.position = createVector(x,y);
   }
 }
